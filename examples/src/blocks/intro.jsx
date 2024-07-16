6a61e4914ab1f9c76e0aad21b5696f6f8f999302
Madeira <marcelo.madeira@calindra.com.br>
2024-07-16 11:13:29 -0300

    feat: :sparkles: error avoiding on Image tag

--- examples/src/blocks/intro.jsx
+++ examples/src/blocks/intro.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Badge, View, Text } from "luminus-daisy"
+import { Badge, View, Text, Image } from "luminus-daisy"
 
 export default function IntroBlock() {
 
   return (
-    <View className="bg-white p-4 ">
+    <View className="bg-white p-4 flex row-auto ">
       <View className="prose prose-slate mb-4 rounded-sm container max-w-5xl m-auto">
         <Text render="h1">Luminus Daisy</Text>
         <Text render="h4" className="font-bold">Luminus is a suite of visual components for building interfaces.</Text>
         <Text render="p">It is the official library used by Eitri for building Eitri-apps.</Text>
         <Badge className="badge-accent badge-outline">Versão 1.0</Badge>
+      </View>
+      <View>
+        <Image src="https://fakeimg.pl/120x120" width={120} height={120} />
       </View>
     </View>
   );
--- src/components/Image/image.tsx
+++ src/components/Image/image.tsx
@@ -6,14 +6,23 @@ interface ImageProps extends Omit<CommonProps, 'children'> {
   alt?: string;
   loading?: 'eager' | 'lazy'; 
   decoding?: 'sync' | 'async';
-  width?: number;
-  height?: number;
+  width?: string | number;
+  height?: string | number;
   title?: string;
   srcSet?: string;
   sizes?: string;
 }
 
 class Image extends Component<ImageProps> {
+
+  state = {
+    imageError: false,
+  };
+
+  handleError = () => {
+    this.setState({ imageError: true });
+  };
+
   render() {
     const { 
       id,
@@ -29,11 +38,34 @@ class Image extends Component<ImageProps> {
       sizes 
     } = this.props;
 
+    if (this.state.imageError) {
+      return (
+        <div id={id} className={`flex justify-center items-center  bg-neutral-100  ${className}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          data-e="Image"
+          width={width}
+          height={height}
+          viewBox={`0 0 120 120`}
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path className='fill-neutral-100' d="M0 0h120v120H0z" />
+          <path
+            className='fill-neutral-400'
+            fillRule="evenodd"
+            d="M33.25 38.482a2.625 2.625 0 0 1 2.604-2.607h47.292a2.606 2.606 0 0 1 2.604 2.607v42.036a2.625 2.625 0 0 1-2.604 2.607H35.854a2.607 2.607 0 0 1-2.604-2.607V38.482Zm47.25 2.643h-42v36.75l24.392-24.397a2.625 2.625 0 0 1 3.712 0L80.5 67.401V41.125Zm-36.75 10.5a5.25 5.25 0 1 0 10.5 0 5.25 5.25 0 0 0-10.5 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        </div>
+      );
+    }
+
     return (
       <img
         data-e="Image"
         id={id}
-        className={`carousel ${className}`}
+        className={`img ${className}`}
         src={src}
         alt={alt ?? ''} 
         loading={loading} 
@@ -43,6 +75,7 @@ class Image extends Component<ImageProps> {
         title={title}
         srcSet={srcSet}
         sizes={sizes}
+        onError={this.handleError}
       />
     );
   }
