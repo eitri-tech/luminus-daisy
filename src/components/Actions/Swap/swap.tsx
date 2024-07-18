cc847d0e5af5024f987bec2218823702e677bbd0
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2024-07-18 12:39:52 -0300

    refactor: Adicionado sub componentes SwapOn and SwapOff e função onChange

--- src/components/Actions/Swap/swap.tsx
+++ src/components/Actions/Swap/swap.tsx
@@ -1,15 +1,48 @@
+import { ChangeEvent } from 'react';
 import {Component} from 'react';
 import CommonProps from '../../commonProps';
 
-interface SwapProps extends CommonProps { }
+interface SwapProps extends CommonProps {
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-class Swap extends Component<SwapProps> {
+interface SwapItemProps extends CommonProps { }
+
+
+class SwapOn extends Component<SwapItemProps> {
     render() {
         const {children, className = '', id} = this.props;
         return (
-            <label data-e="Swap" id={id} className={`swap ${className}`} >
-                <input type="checkbox" />
+            <div data-e="SwapOn" id={id} className={`swap-on ${className}`}>
                 {children}
+            </div>
+        );
+    }
+}
+
+class SwapOff extends Component<SwapItemProps> {
+    render() {
+        const {children, className = '', id} = this.props;
+        return (
+            <div data-e="SwapOff" id={id} className={`swap-off ${className}`}>
+                {children}
+            </div>
+        );
+    }
+}
+
+
+class Swap extends Component<SwapProps> {
+
+    static readonly On = SwapOn;
+    static readonly Off = SwapOff;
+
+    render() {
+        const {children, className = '', id, onChange} = this.props;
+        return (
+            <label data-e="Swap" id={id} className={`swap ${className}`} >
+                <input type="checkbox" onChange={onChange} />
+                {children}      
             </label>
         );
     }
