85b0709a4f3b70b30d82f0a5b38abc793121caec
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2024-07-25 11:51:11 -0300

    adicionado for tab

--- examples/src/blocks/layout/indicator.jsx
+++ examples/src/blocks/layout/indicator.jsx
@@ -33,7 +33,12 @@ export default function IndicatorBlock() {
 
                 <Text render="p">For tab</Text>
                 <Indicator>
-
+                    <a className="tab">Messages</a>
+                    <a className="indicator tab tab-active">
+                        Notifications
+                        <span className="indicator-item badge">8</span>
+                    </a>
+                    <a className="tab">Requests</a>
                 </Indicator>
 
                 <Text render="p">For avatar</Text>
