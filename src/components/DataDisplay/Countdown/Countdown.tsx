cdd42a41c90f5e2a4332959e0a8d3c4e130e600a
Gabriel Fonseca <gabriel.silva@calindra.com.br>
2024-10-02 15:58:27 -0300

    fix: type

--- src/components/DataDisplay/Countdown/Countdown.tsx
+++ src/components/DataDisplay/Countdown/Countdown.tsx
@@ -1,26 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import CommonProps from "../../commonProps";
 
 interface CountdownProps extends CommonProps {
-    counter: number;
+  counter: number;
 }
 
 class Countdown extends Component<CountdownProps> {
-    render() {
+  render() {
+    const { counter, className = "" } = this.props;
 
-        const {
-            counter,
-            className = ""
-        } = this.props
-
-        return (
-            <span
-                className={`countdown ${className}`}
-            >
-                <span style={{"--value": `${counter}`}}></span>
-            </span>
-        );
-    }
+    return (
+      <span className={`countdown ${className}`}>
+        <span style={{ "--value": `${counter}` } as never}></span>
+      </span>
+    );
+  }
 }
 
 export default Countdown;
